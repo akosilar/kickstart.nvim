0de68d73ebde5e694ca0b67693f10bec0bc08146
akosilar <janlarsaustero@gmail.com>
2025-05-24 23:53:58 -0400

    first commit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,36 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'ThePrimeagen/harpoon',
+    config = function()
+      require('harpoon').setup()
+
+      vim.keymap.set('n', '<leader>a', function()
+        require('harpoon.mark').add_file()
+      end, { desc = 'Harpoon: Add file' })
+      vim.keymap.set('n', '<C-e>', function()
+        require('harpoon.ui').toggle_quick_menu()
+      end, { desc = 'Harpoon: Toggle menu' })
+      vim.keymap.set('n', '<C-h>', function()
+        require('harpoon.ui').nav_file(1)
+      end)
+      vim.keymap.set('n', '<C-j>', function()
+        require('harpoon.ui').nav_file(2)
+      end)
+      vim.keymap.set('n', '<C-k>', function()
+        require('harpoon.ui').nav_file(3)
+      end)
+      vim.keymap.set('n', '<C-l>', function()
+        require('harpoon.ui').nav_file(4)
+      end)
+    end,
+  },
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    end,
+  },
+}
